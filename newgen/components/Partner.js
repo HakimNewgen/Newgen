0a24f06cd5b0e9c398bef2ae8ee0a057df925b9e
HakimNewgen <87693425+HakimNewgen@users.noreply.github.com>
2021-07-22 15:08:45 +0100

    Update Partner.js

--- newgen/components/Partner.js
+++ newgen/components/Partner.js
@@ -1,4 +1,4 @@
-
+import Image from 'next/image'
 
 function Partner() {
     return (
@@ -6,100 +6,100 @@ function Partner() {
             <div class="slider">
                 <ul className="bg-gray-500">
                     <li>
-                        <img src="https://i.ibb.co/QcdzgD7/unnamed-removebg-preview.png"/>
+                        <Image src="https://i.ibb.co/QcdzgD7/unnamed-removebg-preview.png" height={100} width={100}/>
                     </li>
                    
                      <li>
-                        <img src="https://i.ibb.co/yhFB5Bx/Wii-logo-880x660-removebg-preview.png"/>
+                        <Image src="https://i.ibb.co/yhFB5Bx/Wii-logo-880x660-removebg-preview.png" height={100} width={100}/>
                      </li>
                      <li>
-                        <img src="https://i.ibb.co/QcdzgD7/unnamed-removebg-preview.png"/>
+                        <Image src="https://i.ibb.co/QcdzgD7/unnamed-removebg-preview.png" height={100} width={100}/>
                      </li>
                      <li>
-                        <img src="https://i.ibb.co/QcdzgD7/unnamed-removebg-preview.png"/>
+                        <Image src="https://i.ibb.co/QcdzgD7/unnamed-removebg-preview.png" height={100} width={100}/>
                      </li>
                     <li>
-                        <img src="https://i.ibb.co/yhFB5Bx/Wii-logo-880x660-removebg-preview.png" />
+                        <Image src="https://i.ibb.co/yhFB5Bx/Wii-logo-880x660-removebg-preview.png" height={100} width={100} />
                     </li>
 
                     <li>
-                        <img src="https://i.ibb.co/pxxLfm1/logo-amazon-404px-grey-removebg-preview.png" />
+                        <Image src="https://i.ibb.co/pxxLfm1/logo-amazon-404px-grey-removebg-preview.png" height={100} width={100} />
                     </li>
 
                   
 
                     <li>
-                        <img src="https://i.ibb.co/QcdzgD7/unnamed-removebg-preview.png" />
+                        <Image src="https://i.ibb.co/QcdzgD7/unnamed-removebg-preview.png" height={100} width={100} />
                     </li>
 
                     <li>
-                        <img src="https://i.ibb.co/pxxLfm1/logo-amazon-404px-grey-removebg-preview.png" />
+                        <Image src="https://i.ibb.co/pxxLfm1/logo-amazon-404px-grey-removebg-preview.png" height={100} width={100} />
                     </li>
 
                     <li>
-                        <img src="https://i.ibb.co/QcdzgD7/unnamed-removebg-preview.png" />
+                        <Image src="https://i.ibb.co/QcdzgD7/unnamed-removebg-preview.png" height={100} width={100} />
                     </li>
 
                     <li>
-                        <img src="https://i.ibb.co/QcdzgD7/unnamed-removebg-preview.png" />
+                        <Image src="https://i.ibb.co/QcdzgD7/unnamed-removebg-preview.png" height={100} width={100} />
                     </li>
 
                     <li>
-                        <img src="https://i.ibb.co/pxxLfm1/logo-amazon-404px-grey-removebg-preview.png" />
+                        <Image src="https://i.ibb.co/pxxLfm1/logo-amazon-404px-grey-removebg-preview.png" height={100} width={100} />
                     </li>
 
                     <li>
-                        <img src="https://i.ibb.co/QcdzgD7/unnamed-removebg-preview.png" />
+                        <Image src="https://i.ibb.co/QcdzgD7/unnamed-removebg-preview.png" height={100} width={100} />
                     </li>
 
 
 
                     <li>
-                        <img src="https://i.ibb.co/QcdzgD7/unnamed-removebg-preview.png" />
+                        <Image src="https://i.ibb.co/QcdzgD7/unnamed-removebg-preview.png" height={100} width={100} />
                     </li>
                     
 
                     <li>
-                        <img src="https://i.ibb.co/yhFB5Bx/Wii-logo-880x660-removebg-preview.png" />
+                        <Image src="https://i.ibb.co/yhFB5Bx/Wii-logo-880x660-removebg-preview.png" height={100} width={100} />
                     </li>
                     <li>
-                        <img src="https://i.ibb.co/QcdzgD7/unnamed-removebg-preview.png" />
+                        <Image src="https://i.ibb.co/QcdzgD7/unnamed-removebg-preview.png" height={100} width={100} />
                     </li>
                     <li>
-                        <img src="https://i.ibb.co/QcdzgD7/unnamed-removebg-preview.png" />
+                        <Image src="https://i.ibb.co/QcdzgD7/unnamed-removebg-preview.png" height={100} width={100} />
                     </li>
                     <li>
-                        <img src="https://i.ibb.co/yhFB5Bx/Wii-logo-880x660-removebg-preview.png" />
+                        <Image src="https://i.ibb.co/yhFB5Bx/Wii-logo-880x660-removebg-preview.png" height={100} width={100} />
                     </li>
 
                     <li>
-                        <img src="https://i.ibb.co/pxxLfm1/logo-amazon-404px-grey-removebg-preview.png" />
+                        <Image src="https://i.ibb.co/pxxLfm1/logo-amazon-404px-grey-removebg-preview.png" height={100} width={100} />
                     </li>
 
                  
 
                     <li>
-                        <img src="https://i.ibb.co/QcdzgD7/unnamed-removebg-preview.png" />
+                        <Image src="https://i.ibb.co/QcdzgD7/unnamed-removebg-preview.png" height={100} width={100} />
                     </li>
 
                     <li>
-                        <img src="https://i.ibb.co/pxxLfm1/logo-amazon-404px-grey-removebg-preview.png" />
+                        <Image src="https://i.ibb.co/pxxLfm1/logo-amazon-404px-grey-removebg-preview.png" height={100} width={100} />
                     </li>
 
                     <li>
-                        <img src="https://i.ibb.co/QcdzgD7/unnamed-removebg-preview.png" />
+                        <Image src="https://i.ibb.co/QcdzgD7/unnamed-removebg-preview.png" height={100} width={100} />
                     </li>
 
                     <li>
-                        <img src="https://i.ibb.co/QcdzgD7/unnamed-removebg-preview.png" />
+                        <Image src="https://i.ibb.co/QcdzgD7/unnamed-removebg-preview.png" height={100} width={100} />
                     </li>
 
                     <li>
-                        <img src="https://i.ibb.co/pxxLfm1/logo-amazon-404px-grey-removebg-preview.png" />
+                        <Image src="https://i.ibb.co/pxxLfm1/logo-amazon-404px-grey-removebg-preview.png" height={100} width={100} />
                     </li>
 
                     <li>
-                        <img src="https://i.ibb.co/QcdzgD7/unnamed-removebg-preview.png" />
+                        <Image src="https://i.ibb.co/QcdzgD7/unnamed-removebg-preview.png" height={100} width={100} />
                     </li>
 
                                        
@@ -108,7 +108,7 @@ function Partner() {
             
 
             </div>
-            <div className=" h-96   grid place-items-center " style={{ background: 'url("https://i.ibb.co/WDBSVX4/bg-25.jpg") ', backgroundSize: 'cover', backgroundRepeat: 'no-repeat' }}>
+            <div className=" h-96   grid place-items-center " style={{ background: `url(https://i.ibb.co/WDBSVX4/bg-25.jpg) ` , backgroundSize: 'cover', backgroundRepeat: 'no-repeat' }}>
                 <div className=" ">
                     <h3 className="font-bold text-4xl text-center text-white    ">Great things in business are <br />
                         never done by one person. <br />
