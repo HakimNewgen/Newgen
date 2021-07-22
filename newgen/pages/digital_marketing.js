6f1ca08ac2379b30139080113d6113d07d0bb00c
HakimNewgen <87693425+HakimNewgen@users.noreply.github.com>
2021-07-22 15:30:03 +0100

    Update digital_marketing.js

--- newgen/pages/digital_marketing.js
+++ newgen/pages/digital_marketing.js
@@ -2,6 +2,8 @@ import Banner from "../components/Banner";
 import Image from "next/image"
 import DigitalService from "../components/DigitalService";
 import Latest from "../components/Latest";
+import Footer from "../components/Footer";
+ 
 
 function digital_marketing() {
     return (
@@ -21,22 +23,9 @@ une base de fans dédiés à travers un Marketing organique" />
 
 
 
-            <div className=" flex w-9/12 mx-auto justify-between">
-                <DigitalService icon="https://i.ibb.co/SBDhQyF/Capture.png" title="Email Marketing" text="Découvrez avec nous Le moyen le plus efficace d'engager et de réengager le dialogue avec les anciens visiteurs qui ont montré de l'intérêt pour votre marque et ont laissé leurs coordonnées." />
-                <DigitalService title="Contenu" icon="https://i.ibb.co/smzW3rq/Capture.png" text="Découvrez un contenu original et attrayant qui augmentera la notoriété de votre marque et stimulera vos efforts de référencement, ce qui signifie plus de clics, de pistes et de conversions.
-" />
-                <DigitalService title="Optimisation des taux de conversion" icon="https://i.ibb.co/Ph3vRXz/Capture.png" text="Transformez les visiteurs occasionnels de votre site Web en clients payants... ou même en défenseurs de la marque. Vous pouvez obtenir plus de pistes et de ventes à partir du trafic de votre site Web lorsqu'il est entièrement optimisé pour les conversions." />
-
-
-            </div>
-            <div className=" flex w-9/12 mx-auto justify-between">
-                <DigitalService icon="https://i.ibb.co/q9jJmjn/Capture.png" title="Suivi de la mise en œuvre" text="Vérifiez que vous suivez correctement toutes vos données de conversion et d'analyse. Pour réussir, la gestion et l'optimisation des campagnes doivent s'appuyer sur les bonnes données." />
-                <DigitalService title="ASO : App Store Optimisation" icon="https://i.ibb.co/93t52cv/Capture.png" text="Rendez votre application plus attirante pour les utilisateurs qui visitent directement les magasins d'applications." />
-                
-
-            </div>
-
+           
             <Latest />
+            <Footer/>
         </div>
     )
 }
