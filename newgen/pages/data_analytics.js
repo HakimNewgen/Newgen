a9b04c61ba7c656b38fcd3eb8a4448e0e3764e63
HakimNewgen <87693425+HakimNewgen@users.noreply.github.com>
2021-07-22 14:31:41 +0100

    Update data_analytics.js

--- newgen/pages/data_analytics.js
+++ newgen/pages/data_analytics.js
@@ -4,7 +4,7 @@ import Banner from "../components/Banner";
 function data_analytics() {
     return (
         <div>
-            <Banner source="https://i.ibb.co/b69D6Q6/Data-banner.png" navbar="blue-900" text="Solutions numériques avec une approche de développement centrée sur l'utilisateur" font={"text-3xl"} size="cover" size="contain" color={"text-blue-900"}/>
+            <Banner source="https://i.ibb.co/b69D6Q6/Data-banner.png" navbar="blue-900" text="Solutions numériques avec une approche de développement centrée sur l'utilisateur" font={"text-3xl"}  size="contain" color={"text-blue-900"}/>
            
 
             
