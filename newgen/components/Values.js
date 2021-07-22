de810f9d1942570296f61956a630fd8d4e476058
HakimNewgen <87693425+HakimNewgen@users.noreply.github.com>
2021-07-22 15:01:43 +0100

    Update Values.js

--- newgen/components/Values.js
+++ newgen/components/Values.js
@@ -15,14 +15,14 @@ function Values() {
                 <div className="w-72 text-center  ">
                     <LinkIcon className=" w-10 text-blue-900 text-center ml-32  "/>
                     <h4 className="font-bold text-blue-900 text-xl mb-6" >Confiance</h4>
-                    <p>Nous sommes persuadés que la confiance amène à  des collaborations pérenne dans le temps. Par conséquent, établir et maintenir ’une relation de confiance avec nos collaborateurs ainsi que nos clients est un impératif.</p>
+                    <p>Nous sommes persuadés que la confiance amène à  des collaborations pérenne dans le temps. Par conséquent, établir et maintenir une relation de confiance avec nos collaborateurs ainsi que nos clients est un impératif.</p>
 
               </div>
 
                 <div className="w-72 text-center ">
                     <FaUserTie className=" text-5xl text-blue-900 text-center ml-32"  />
                     <h4 className="font-bold text-blue-900 text-xl mb-6" >Professionnalisme</h4>
-                    <p>Nous avons  un grand sens d’engagement envers nos clients. Nos équipes s'investissent pleinement dans leur travail quotidien dans le but de livrer des produits de qualité dans les temps.</p>
+                    <p>Nous avons  un grand sens d’engagement envers nos clients. Nos équipes investissent pleinement dans leur travail quotidien dans le but de livrer des produits de qualité dans les temps.</p>
 
                 </div>
 
